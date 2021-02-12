0544f0634bf3e019e0bc1c41a1765bd6625832a1
zhrcosta <guilherme.di.aquino@gmail.com>
2021-02-12 15:32:15 -0300

    primeiro teste ok

--- isPrime.py
+++ isPrime.py
@@ -7,5 +7,27 @@
 def isprime(num):
 
     divisors = []
+    d = 0
+    b = None
 
-    return divisors
+    for i in range(1, (num//2)+1):
+        if num % i == 0:
+            divisors.append(i)
+            d += 1
+
+    divisors.append(num)
+    d += 1
+
+    if d == 2:
+        b = True
+    else:
+        b = False
+
+    return divisors, d, b
+
+
+divs, amount, value = isprime(23)
+
+print(divs)
+print(amount)
+print(value)

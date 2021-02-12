ba089ed0fd09268fc4442b5d08cbf9440376a5ea
zhrcosta <guilherme.di.aquino@gmail.com>
2021-02-12 17:35:37 -0300

    Teste inicial

---
+++ primeFactorization.py
@@ -0,0 +1,39 @@
+"""
+    Função para decompor um número inteiro em fatores primos,
+    retornando uma lista
+"""
+
+
+def primeFac(num):
+
+    def isprime(prime):
+
+        divisors = 0
+
+        for i in range(1, (prime//2)+1):
+            if prime % i == 0:
+                divisors += 1
+
+        divisors += 1
+
+        value = True if divisors == 2 else False
+
+        return value
+
+    fac = []
+    val = num
+    divisor = 2
+
+    while val > 1:
+
+        if isprime(divisor):
+            if val % divisor == 0:
+                val = val // divisor
+                fac.append(divisor)
+        else:
+            divisor += 1
+
+    return fac
+
+
+print(primeFac(72))
